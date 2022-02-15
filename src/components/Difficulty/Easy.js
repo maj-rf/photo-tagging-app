2785b79e4e5860524e9b708907bc998f4a6ee77d
bananabread08 <rmkfermin@gmail.com>
2022-02-15 18:08:17 +0800

    added mouse coord tracker in Easy

--- src/components/Difficulty/Easy.js
+++ src/components/Difficulty/Easy.js
@@ -1,3 +1,31 @@
+import cartoonpic from '../../assets/cartoonpic.jpeg';
+import styled from 'styled-components';
+
+const StyledSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const StyledImg = styled.img`
+  max-height: 100vh;
+  max-width: 100vw;
+`;
+const StyledDiv = styled.div`
+  border: 3px solid #fff;
+`;
+
 export default function Easy() {
-  return <div>Easy</div>;
+  const onMouseDown = (e) =>
+    console.log(
+      `clientX: ${e.nativeEvent.offsetX}, clientY: ${e.nativeEvent.offsetY}`
+    );
+
+  return (
+    <StyledSection>
+      <h1>Easy</h1>
+      <StyledDiv>
+        <StyledImg src={cartoonpic} onMouseDown={(e) => onMouseDown(e)} />
+      </StyledDiv>
+    </StyledSection>
+  );
 }
