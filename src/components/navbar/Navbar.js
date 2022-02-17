64aeb7b81bc35811dbf78a056c79dcdaface3c7f
bananabread08 <rmkfermin@gmail.com>
2022-02-17 11:16:55 +0800

    added dropdown toggling on click and see choices

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -13,6 +13,7 @@ const StyledNav = styled.nav`
   position: fixed;
   top: 0;
   width: 100%;
+  z-index: 1;
 `;
 
 const StyledUl = styled.ul`
--- src/components/pages/Easy.js
+++ src/components/pages/Easy.js
@@ -1,6 +1,6 @@
 import cartoonpic from '../../assets/cartoonpic.jpeg';
 import styled from 'styled-components';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const StyledSection = styled.section`
   display: flex;
@@ -16,18 +16,88 @@ const StyledImg = styled.img`
 `;
 const StyledDiv = styled.div`
   border: 3px solid #fff;
+  position: relative;
 `;
 
-export default function Easy() {
-  const onMouseDown = (e) => {
-    const xCoord = Math.round(
+const DropDownContainer = styled('div')`
+  margin: 0 auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const DropDownListContainer = styled('div')``;
+
+const DropDownList = styled('ul')`
+  padding: 0;
+  margin: 0;
+  padding-left: 1em;
+  padding-right: 1em;
+  background: #ffffff;
+  border: 2px solid #e5e5e5;
+  box-sizing: border-box;
+  color: black;
+  font-size: calc(1rem + ((1vw - 0.48em) * 1.3889));
+  font-weight: 500;
+  &:first-child {
+    padding-top: 0.8em;
+  }
+`;
+
+const ListItem = styled('li')`
+  list-style: none;
+  margin-bottom: 0.8em;
+  cursor: pointer;
+
+  &:hover {
+    color: tomato;
+  }
+`;
+
+export default function Easy(props) {
+  const [currentTime, setCurrentTime] = useState(0);
+  const [choiceCoords, setChoiceCoords] = useState(null);
+  const choices = [...props.easyItems].slice(3);
+  const [isOpen, setIsOpen] = useState(false);
+  // get currentTime on component mount
+  useEffect(() => {
+    console.log(getCurrentTime());
+  }, []);
+
+  function convertCoordstoPercent(e) {
+    let x = Math.round(
       (e.nativeEvent.offsetX / e.nativeEvent.target.offsetWidth) * 100
     );
-    const yCoord = Math.round(
+    let y = Math.round(
       (e.nativeEvent.offsetY / e.nativeEvent.target.offsetHeight) * 100
     );
-    console.log(xCoord, yCoord);
-  };
+    return { x, y };
+  }
+
+  function getCurrentTime() {
+    let today = new Date();
+    let totalSeconds =
+      today.getHours() * 60 * 60 + today.getMinutes() * 60 + today.getSeconds();
+    setCurrentTime(totalSeconds);
+    return totalSeconds;
+  }
+
+  function toggling(e) {
+    const dropdown = document.querySelector('.dropdown');
+    let coords = convertCoordstoPercent(e);
+    if (coords.x > 80) coords.x = 65;
+    if (coords.y > 80) coords.y = 65;
+    dropdown.style.left = `${coords.x}%`;
+    dropdown.style.top = `${coords.y}%`;
+    setIsOpen(!isOpen);
+  }
+
+  function pointImage(e) {
+    toggling(e);
+    let coords = convertCoordstoPercent(e);
+    console.log(coords.x, coords.y);
+    setChoiceCoords((prevState) => (prevState = { ...coords }));
+  }
 
   return (
     <StyledSection>
@@ -36,8 +106,26 @@ export default function Easy() {
         <StyledImg
           id="easyPic"
           src={cartoonpic}
-          onMouseDown={(e) => onMouseDown(e)}
+          onMouseDown={(e) => pointImage(e)}
         />
+        <DropDownContainer className="dropdown">
+          {isOpen && (
+            <DropDownListContainer>
+              <DropDownList>
+                {choices.map((choice) => {
+                  return (
+                    <ListItem
+                      key={choice.name + choice.x}
+                      onClick={() => console.log(choiceCoords)}
+                    >
+                      {choice.name}
+                    </ListItem>
+                  );
+                })}
+              </DropDownList>
+            </DropDownListContainer>
+          )}
+        </DropDownContainer>
       </StyledDiv>
     </StyledSection>
   );
