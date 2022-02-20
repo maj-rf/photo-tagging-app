403c88668beced08a77d2ea3d87820c3f9f81e78
bananabread08 <rmkfermin@gmail.com>
2022-02-20 10:44:39 +0800

    added more items to find

--- src/components/helpers/data.js
+++ src/components/helpers/data.js
@@ -1,57 +1,60 @@
 import finn from '../../assets/finn.png';
 import patrick from '../../assets/patrick.png';
-import pikachu from '../../assets/pikachu.png';
+import sans from '../../assets/sans.png';
 import morty from '../../assets/morty.png';
 import jack from '../../assets/jack.png';
 import keanu from '../../assets/keanu.png';
+import yoshi from '../../assets/yoshi.png';
+import goku from '../../assets/goku.png';
+
 const easy = [
   {
     name: 'Patrick',
     img: patrick,
     id: 'patrick',
     url: '',
-    x: 76,
-    y: 64,
   },
   {
     name: 'Morty',
     img: morty,
     id: 'morty',
     url: '',
-    x: 33,
-    y: 37,
-  },
-  {
-    name: 'Pikachu',
-    img: pikachu,
-    id: 'pikachu',
-    url: '',
-    x: 41,
-    y: 74,
   },
   {
     name: 'Samurai Jack',
     img: jack,
     id: 'jack',
     url: '',
-    x: 93,
-    y: 16,
   },
   {
     name: 'Keanu Reeves',
     img: keanu,
     id: 'keanu',
     url: '',
-    x: 21,
-    y: 21,
   },
   {
     name: 'Finn the Human',
     img: finn,
     id: 'finn',
     url: '',
-    x: 14,
-    y: 54,
+  },
+  {
+    name: 'Sans',
+    img: sans,
+    id: 'sans',
+    url: '',
+  },
+  {
+    name: 'Yoshi',
+    img: yoshi,
+    id: 'yoshi',
+    url: '',
+  },
+  {
+    name: 'Goku',
+    img: goku,
+    id: 'goku',
+    url: '',
   },
 ];
 
